60a7526ec30a66e294a953f4e875c76c2fde2252
LesserjL <107967719+LesserjL@users.noreply.github.com>
2023-11-12 11:56:19 -0600

    Create QuestionNode.java

---
+++ QuestionNode.java
@@ -0,0 +1,46 @@
+// The QuestionNode class is used in conjunction with the QuestionTree.  Leaf
+// nodes store the name of an object and branch nodes store a question.
+
+public class QuestionNode {
+    /**Create 3 instance variables to store the data and references to the left and right nodes. For this project you can make them public :) **/
+    public String text;
+    public QuestionNode left;
+    public QuestionNode right;
+    
+    /**Create a Constructure with a single parameter for the data that sets the left and right nodes to null**/
+    public QuestionNode(String text){
+        this.text = text;
+        left = null;
+        right = null;
+    }
+    
+    /**Create another constructor that takes data (text) and left/right references**/
+    public QuestionNode(String text, QuestionNode left, QuestionNode right){
+        this.text = text;
+        this.left = left;
+        this.right = right;
+    }
+
+    public String getText(){
+        return text;
+    }
+    /** post: returns whether or not a node is a leaf node**/
+    public boolean isLeaf() {
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null){
+            return true;
+        }
+        return false; /**Update this method to test whether or not this node is a left node**/
+    }
+    public QuestionNode getLeft()
+    {
+        return left;
+    }
+
+    public QuestionNode getRight()
+    {
+        return right;
+    }
+}
